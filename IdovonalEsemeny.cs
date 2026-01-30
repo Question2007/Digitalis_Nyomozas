f86c646b3a4f0e96f546fd938249f6659f606dd6
Zétény <karsai.zeteny@petrik.hu>
2026-01-30 10:03:37 +0100

    IdovonalEsemeny osztaly kesz.

---
+++ IdovonalEsemeny.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digitalis_Nyomozas
+{
+	internal class IdovonalEsemeny
+	{
+		private DateTime datum;
+		private string esemenyLeiras;
+
+		public IdovonalEsemeny(DateTime datum, string esemenyLeiras)
+		{
+			this.datum = datum;
+			this.esemenyLeiras = esemenyLeiras;
+		}
+
+		public DateTime Datum { get => datum; set => datum = value; }
+		public string EsemenyLeiras { get => esemenyLeiras; set => esemenyLeiras = value; }
+
+		public override string ToString() {
+			return $"Dátum: {this.datum} Esemény leírás: {this.esemenyLeiras}";
+		}
+	}
+}
