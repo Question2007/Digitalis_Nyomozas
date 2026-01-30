1da603b4266d70b6032a3ebe8a6749ccaaf9bad2
Zétény <karsai.zeteny@petrik.hu>
2026-01-30 09:06:35 +0100

    Szemely osztaly kesz.

---
+++ Szemely.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digitalis_Nyomozas
+{
+	internal class Szemely
+	{
+		private string nev;
+		private int eletkor;
+		private string megjegyzes;
+
+		public Szemely(string nev, int eletkor, string megjegyzes)
+		{
+			this.nev = nev;
+			this.eletkor = eletkor;
+			this.megjegyzes = megjegyzes;
+		}
+
+		public string Nev { get => nev; set => nev = value; }
+		public int Eletkor { get => eletkor; set => eletkor = value; }
+		public string Megjegyzes { get => megjegyzes; set => megjegyzes = value; }
+
+		public override string ToString()
+		{
+			return $"Név: {this.nev} Életkor: {this.eletkor} Megjegyzés: {this.megjegyzes}";
+		}
+	}
+}
