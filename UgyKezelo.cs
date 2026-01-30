43e537884611e70ea2ff07f4d34a397c81e6370e
Zétény <karsai.zeteny@petrik.hu>
2026-01-30 10:26:17 +0100

    UgyKezelo UjUgyLetrehozasa és UgyekListazasa kesz marcsak a Szemelyek es Bizonyitekok hozzarendelese kell.

---
+++ UgyKezelo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digitalis_Nyomozas
+{
+	internal class UgyKezelo
+	{
+		private List<Ugy> ugyek;
+
+		public UgyKezelo(List<Ugy> ugyek)
+		{
+			this.ugyek = new List<Ugy>();
+		}
+
+		internal List<Ugy> Ugyek { get => ugyek; set => ugyek = value; }
+
+		public void UjUgyLetrehozasa(Ugy u)
+		{
+			ugyek.Add(u);
+		}
+
+		public void UgyekListazasa() 
+		{
+			foreach (var item in ugyek)
+			{
+				Console.WriteLine(item);
+			}
+		}
+	}
+}
