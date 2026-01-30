b978bfc7ec991693ffb262ea3d2f455c9403e47c
Zétény <karsai.zeteny@petrik.hu>
2026-01-30 10:32:47 +0100

    Donteshozo metodus osztaly kesz.

---
+++ Donteshozo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digitalis_Nyomozas
+{
+	internal class Donteshozo
+	{
+		public Donteshozo(Gyanusitott gy, Bizonyitek b) {
+			if (b.MegbizhatosagiErtek >= 3)
+			{
+				gy.GyanusitottsagiSzint += 10;
+			}
+			if (gy.GyanusitottsagiSzint >= 75)
+			{
+				Console.WriteLine("Figyelem elértél egy Gyanusítottsági szint küszöbértéket!!!");
+			}
+		}
+	}
+}
