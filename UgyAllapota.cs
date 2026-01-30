561e07bcee5b6d0c9b0c1c32ba6164cc5b47a54e
Zétény <karsai.zeteny@petrik.hu>
2026-01-30 09:30:45 +0100

    UgyAllapota osztaéy  felig kesz.

---
+++ UgyAllapota.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digitalis_Nyomozas
+{
+	internal class UgyAllapota
+	{
+		private string aktualisStatusz;
+
+		public UgyAllapota(string aktualisStatusz)
+		{
+			this.aktualisStatusz = aktualisStatusz;
+		}
+
+		public string AktualisStatusz { get => aktualisStatusz; set => aktualisStatusz = value; }
+
+		public void AllapotValtoztatas(string allapot)
+		{
+			this.aktualisStatusz = allapot;
+		}
+	} 
+}
