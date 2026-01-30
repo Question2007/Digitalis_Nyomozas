241464962419f0541588ebd9748f0fbdc0b604da
Zétény <karsai.zeteny@petrik.hu>
2026-01-30 10:19:27 +0100

    BizonyitekKezelo seged metódus osztaly kesz.

--- BizonyitekKezelo.cs
+++ BizonyitekKezelo.cs
@@ -8,5 +8,23 @@ namespace Digitalis_Nyomozas
 {
 	internal class BizonyitekKezelo
 	{
+		private List<Bizonyitek> bizonyitekok;
+
+		public BizonyitekKezelo(List<Bizonyitek> bizonyitekok)
+		{
+			this.bizonyitekok = new List<Bizonyitek>();
+		}
+
+		internal List<Bizonyitek> Bizonyitekok { get => bizonyitekok; set => bizonyitekok = value; }
+
+		public void BizonyitekHozzaadasa(Bizonyitek b)
+		{
+			bizonyitekok.Add(b);
+		}
+
+		public void BizonyitekTorlese(Bizonyitek b)
+		{
+			bizonyitekok.Remove(b);
+		}
 	}
 }
