50aa96d0eb9431f74403bdbe11da4ce54d4c62ec
Zétény <karsai.zeteny@petrik.hu>
2026-01-30 10:44:53 +0100

    BizonyitekKezelo megnezi hogy tartalmazza-e a bizonyitekokat vagy sem es figyelmeztet es hozzaadja raktarhoz ha a bizonyitekokban benne van es kiveszi ha a raktarban van benne.

--- BizonyitekKezelo.cs
+++ BizonyitekKezelo.cs
@@ -9,22 +9,45 @@ namespace Digitalis_Nyomozas
 	internal class BizonyitekKezelo
 	{
 		private List<Bizonyitek> bizonyitekok;
-
+		private List<Bizonyitek> raktar;
 		public BizonyitekKezelo(List<Bizonyitek> bizonyitekok)
 		{
 			this.bizonyitekok = new List<Bizonyitek>();
+			this.raktar = new List<Bizonyitek>();
 		}
 
 		internal List<Bizonyitek> Bizonyitekok { get => bizonyitekok; set => bizonyitekok = value; }
+		internal List<Bizonyitek> Raktar { get => raktar; set => raktar = value; }
 
 		public void BizonyitekHozzaadasa(Bizonyitek b)
 		{
-			bizonyitekok.Add(b);
+			if (raktar.Contains(b)) { 
+				bizonyitekok.Add(b);
+				raktar.Remove(b);
+			}
+			else if (bizonyitekok.Contains(b)) {
+				Console.WriteLine("Ez már benne van a bizonyítékokban!");
+			}
+			else
+			{
+				bizonyitekok.Add(b);
+			}
+			
 		}
 
-		public void BizonyitekTorlese(Bizonyitek b)
+		public Bizonyitek BizonyitekTorlese(Bizonyitek b)
 		{
-			bizonyitekok.Remove(b);
+			if (!bizonyitekok.Contains(b))
+			{
+				Console.WriteLine("Nincs ilyen bizonyítékok között!");
+			}
+			else
+			{
+				raktar.Add(b);
+				bizonyitekok.Remove(b);
+			}
+
+			return b;
 		}
 	}
 }
